6055e9b4851482ca519e8243bd3a16ac753ff653
fanchuanster@gmail.com <fanchuanster@gmail.com>
2018-01-29 21:38:11 +0800

    work of toay

--- practical_ml_teaching.py
+++ practical_ml_teaching.py
@@ -11,26 +11,29 @@ df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Open'] * 100.0
 df['Change_PCT'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
 df = df[['Adj. Close', 'HL_PCT', 'Change_PCT', 'Adj. Volume']]
+df.fillna(-99999, inplace=True)
 
 forecast_col = 'Adj. Close'
 
-
-df['label'] = df[forecast_col]
-
 forecast_out = 3
-
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
-
-print(df.head())
 
 X = np.array(df.drop(['label'], 1))
+X = preprocessing.scale(X)
+
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+
+df.dropna(inplace=True)
 y = np.array(df['label'])
 
-X = preprocessing.scale(X)
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
-
+clf = LinearRegression(n_jobs=-1)
+#clf = svm.SVR(kernel='poly')
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
-print(accuracy)
+
+forecast_set = clf.predict(X_lately)
+print(X_lately, y[:-3], accuracy)
+
